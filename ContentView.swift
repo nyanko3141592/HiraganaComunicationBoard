acb83ae96ea8b83eb866ad5df9fe4c5a4dc5864e
nya3_neko2 <takahashinaoki521@gmail.com>
2023-08-20 05:35:33 +0900

    fix

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,38 @@
 import SwiftUI
 
+struct VerticalSlider: View {
+    @Binding var value: CGFloat
+    var range: ClosedRange<Double>
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                // トラック
+                Rectangle()
+                    .frame(width: 10, height: geometry.size.height) // このwidthが線の太さ
+                    .cornerRadius(5)
+                    .foregroundColor(Color.gray.opacity(0.3))
+
+                // ツマミの部分
+                Circle()
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(Color.white)
+                    .shadow(radius: 2)
+                    .offset(y: -(CGFloat(self.value / self.range.upperBound) * geometry.size.height - (geometry.size.height / 2)))
+                    .gesture(DragGesture().onChanged { gesture in
+                        self.value = Double((geometry.size.height - gesture.location.y) / geometry.size.height) * self.range.upperBound
+                        self.value = min(self.range.upperBound, max(self.range.lowerBound, self.value))
+                    })
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     @State private var labelText: String = ""
-    @State private var fontSize: CGFloat = 16.0
+    @State private var fontSize: CGFloat = 30.0
+    @State private var showYou: Bool = false
+    @State private var showMe: Bool = true
 
     // 例としての二次元配列
     let charList: [[String]] = [
@@ -24,19 +54,86 @@ struct ContentView: View {
         VStack(spacing: 20) {
             // Label
             HStack {
-                ZStack {
-                    RoundedRectangle(cornerRadius: 25)
-                        .fill(Color.white)
-                        .frame(width: .infinity, height: .infinity)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 25)
-                                .stroke(Color.black, lineWidth: 2) // 縁の色と太さを指定
-                        )
-                    Text(labelText == "" ? "テキストを入力" : labelText)
-                        .font(.system(size: fontSize))
-                        .padding()
-                }
                 VStack {
+                    if showYou {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .fill(Color.white)
+                                .frame(width: .infinity, height: .infinity)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .stroke(Color.black, lineWidth: 2) // 縁の色と太さを指定
+                                )
+                            Text(labelText == "" ? "テキストを入力" : labelText)
+                                .font(.system(size: fontSize))
+                                .padding()
+                        }
+                        .rotation3DEffect(
+                                       .degrees(180),
+                                       axis: (x: 0.0, y: 0.0, z: 1.0)
+                                   )
+                    }
+                    if showMe {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 25)
+                                .fill(Color.white)
+                                .frame(width: .infinity, height: .infinity)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 25)
+                                        .stroke(Color.black, lineWidth: 2) // 縁の色と太さを指定
+                                )
+                            Text(labelText == "" ? "テキストを入力" : labelText)
+                                .font(.system(size: fontSize))
+                                .padding()
+                        }
+                    }
+                }
+
+                VStack {
+                    Button(action: {
+                        labelText = ""
+                    }) {
+                        Image(systemName: "xmark") // 再生ボタンのシステムアイコン
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .padding()
+                    .font(.title)
+                    .background(labelText == "" ? Color.gray : Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(8.0)
+                    Button(action: {
+                        if showMe || !showYou{
+                            showYou = !showYou
+                        }
+                    }) {
+                        Image(systemName: showYou ? "person.2.fill" : "person.2") // 再生ボタンのシステムアイコン
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .padding()
+                    .font(.title)
+                    .background(showYou ? Color.green : Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(8.0)
+                    Button(action: {
+                        if !showMe || showYou{
+                            showMe = !showMe
+                        }
+                    }) {
+                        Image(systemName: showMe ? "person.fill" : "person") // 再生ボタンのシステムアイコン
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                    }
+                    .padding()
+                    .font(.title)
+                    .background(showMe ? Color.green : Color.gray)
+                    .foregroundColor(.white)
+                    .cornerRadius(8.0)
+                   
                     Button(action: {
                         fontSize += 2
                     }) {
@@ -46,10 +143,8 @@ struct ContentView: View {
                     }
                     .foregroundColor(.black)
                     .cornerRadius(8.0)
-                    Slider(value: $fontSize, in: 10 ... 100, step: 1)
-                        .frame(width: 20, height: 100)
-                        .rotationEffect(.degrees(90))
-                        .padding()
+                    VerticalSlider(value: $fontSize, range: 0 ... 100)
+                        .frame(width: 30, height: .infinity)
                     Button(action: {
                         fontSize -= 2
                     }) {
@@ -108,35 +203,22 @@ struct ContentView: View {
                 }
             }
             .edgesIgnoringSafeArea(.all) // 画面全体に拡大
-            HStack {
-                Button(action: {
-                    // ボタンがタップされたときのアクション
-                    print("再生ボタンがタップされました")
-                }) {
-                    Image(systemName: "play.fill") // 再生ボタンのシステムアイコン
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                }
-                .padding()
-                .font(.title)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8.0)
-                Button(action: {
-                    labelText = ""
-                }) {
-                    Image(systemName: "xmark") // 再生ボタンのシステムアイコン
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                }
-                .padding()
-                .font(.title)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(8.0)
-            }
+//            HStack {
+//                Button(action: {
+//                    // ボタンがタップされたときのアクション
+//                    print("再生ボタンがタップされました")
+//                }) {
+//                    Image(systemName: "play.fill") // 再生ボタンのシステムアイコン
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: 50, height: 50)
+//                }
+//                .padding()
+//                .font(.title)
+//                .background(Color.blue)
+//                .foregroundColor(.white)
+//                .cornerRadius(8.0)
+//            }
         }
         .padding()
     }
